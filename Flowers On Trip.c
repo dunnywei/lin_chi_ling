1942931d8ccce87212de1a95b5e9932c68b9d6ef
dunnywei <wei_don@yahoo.com>
2019-05-25 20:18:53 -0400

    Update Flowers On Trip.c

--- Flowers On Trip.c
+++ Flowers On Trip.c
@@ -4,3 +4,4 @@ https://www.youtube.com/watch?v=fq25BX_ygNI …
 旅途的花样Flowers On Trip EP.10 20170812
 ->listening to the music->beatiful (37:30)
 ->(52:19) never give up
+->work hard then appreciate (1:16:45)
