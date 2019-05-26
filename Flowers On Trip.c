e61f653358d0d0600613177a790d9c1e56575d77
dunnywei <wei_don@yahoo.com>
2019-05-25 22:07:10 -0400

    Update Flowers On Trip.c

--- Flowers On Trip.c
+++ Flowers On Trip.c
@@ -6,3 +6,9 @@ https://www.youtube.com/watch?v=fq25BX_ygNI …
 ->(52:19) never give up
 ->work hard then appreciate (1:16:45)
 ->holding hand->28:50
+
+ 《旅途的花样》Flowers On Trip EP.11 20170819 Life of Flowers [SMG Official HD]
+ ->saving people from water (7:09)
+  ->drinking jinger coke->(13:14)
+  ->riding bike->(40:46)
+  ->beatiful (1:18:47)
