7736e7febe87a984a7fb8395fa88c1e78e13cd1d
dunnywei <wei_don@yahoo.com>
2019-05-26 20:08:58 -0400

    (36:15)

--- Flowers On Trip.c
+++ Flowers On Trip.c
@@ -12,3 +12,20 @@ https://www.youtube.com/watch?v=fq25BX_ygNI …
   ->drinking jinger coke->(13:14)
   ->riding bike->(40:46)
   ->beatiful (1:18:47)
+ 
+ https://www.youtube.com/watch?v=qkJ1gIoXiOE
+Flowers On Trip EP.12 20170826 Crown Prince of Denmark Attends The Show [SMG Official HD]
+
+->(1:06) exercise
+->(1:54)pretty
+->(3:54)pretty
+->(3:32)marriage
+->(3:37) reaction
+->(5:50) eating
+->(9:14) cute
+->(12:19) shocked and feel amazing
+->(12:34) pretty
+->(15:51) tasting food
+->(19:24) like a little girl
+->(34:29) her opinion about anderso
+->(36:15)
