f4d82f3ba1ef488546113a5f65c6a9ae3254dfdc
dunnywei <wei_don@yahoo.com>
2019-06-01 21:12:27 -0400

    Update Flowers On Trip.c

--- Flowers On Trip.c
+++ Flowers On Trip.c
@@ -31,4 +31,30 @@ Flowers On Trip EP.12 20170826 Crown Prince of Denmark Attends The Show [SMG Off
 ->(34:29) her opinion about anderso
 ->(36:15)eating (39:45)
 ->(40:23)cute
-->(
+->(48:01)Starts 
+->(48:24)saying
+->(50:10)picture was taken
+->(50:48 to 51:06)walking
+->(52:10)helping others
+->(52:30)Cute and suppportive(53:00)
+->(53:33)walking 
+->(54:49)walk back to (56:58)
+->(56:27) where is the most challenging part 
+->(56:38)supportive
+->(56:50)cute
+->(57:17)nice dressing to (57:31) to (57:51)
+->(1:03:37)nice dressing to (1:03:48) to (1:03:51) to (1:04:05) to (1:04:12) to (1:04:16)
+->(1:05:54)cute to (1:06:34)
+->(1:06:57)taking pictures for all 
+->(1:07:27) standing behind to (1:07:34)
+->(1:07:55)cute
+->(1:08:05)taking the car
+->(1:08:25)cute
+->(1:10:49)cute
+->(1:11:03)cute
+->(1:11:57)pretty and going back to sleep
+->(1:13:31)->eating and cute
+
+->Flowers On Trip EP.13 20170902 The Journey Never End
+https://www.youtube.com/watch?v=SHQOloILAmE
+->(0:04)
